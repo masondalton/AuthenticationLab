a0e3e7ba77b4127dd69ebd37819f952bc1845e8d
masondalton <mcoled@byu.edu>
2025-03-31 20:11:46 -0600

    Add authorization to pages

--- RootkitIdentityW25-main/frontend/src/pages/ProductPage.tsx
+++ RootkitIdentityW25-main/frontend/src/pages/ProductPage.tsx
@@ -1,9 +1,11 @@
-import { useNavigate, useParams } from 'react-router-dom';
-import Header from '../components/Header';
-import { useCart } from '../context/CartContext';
-import { CartItem } from '../types/CartItem';
-import { useState } from 'react';
-import CartSummary from '../components/CartSummary';
+import { useNavigate, useParams } from "react-router-dom";
+import Header from "../components/Header";
+import { useCart } from "../context/CartContext";
+import { CartItem } from "../types/CartItem";
+import { useState } from "react";
+import CartSummary from "../components/CartSummary";
+import AuthorizeView, { AuthorizedUser } from "../components/AuthorizeView";
+import Logout from "../components/Logout";
 
 function ProductPage() {
   const navigate = useNavigate();
@@ -11,7 +13,7 @@ function ProductPage() {
   const price = currentRetailPrice ? parseFloat(currentRetailPrice) : 0; // Convert to number or fallback to 0
   if (!rootbeerName || !rootbeerId) {
     throw new Error(
-      'Missing required route parameters: rootBeerName or rootBeerId'
+      "Missing required route parameters: rootBeerName or rootBeerId"
     );
   }
   const { addToCart } = useCart();
@@ -25,31 +27,37 @@ function ProductPage() {
       quantity,
     };
     addToCart(newItem);
-    navigate('/cart');
+    navigate("/cart");
   };
 
   return (
     <>
-      <CartSummary />
-      <Header />
-      <h1>Want a cold refreshing {rootbeerName}?</h1>
-      <h2>Only ${price.toFixed(2)}</h2>
-      <div>
-        <select
-          name="quantity"
-          className="form-select form-select-lg mb-3"
-          onChange={(e) => setQuantity(Number(e.target.value))}
-        >
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-        </select>
-        <button onClick={handleAddToCart}>Add to Cart</button>
-      </div>
+      <AuthorizeView>
+        <span>
+          <Logout>
+            Logout <AuthorizedUser value="email" />
+          </Logout>
+        </span>
+        <CartSummary />
+        <h1>Want a cold refreshing {rootbeerName}?</h1>
+        <h2>Only ${price.toFixed(2)}</h2>
+        <div>
+          <select
+            name="quantity"
+            className="form-select form-select-lg mb-3"
+            onChange={(e) => setQuantity(Number(e.target.value))}
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+          <button onClick={handleAddToCart}>Add to Cart</button>
+        </div>
 
-      <button onClick={() => navigate('/competition')}>Go Back</button>
+        <button onClick={() => navigate("/competition")}>Go Back</button>
+      </AuthorizeView>
     </>
   );
 }
